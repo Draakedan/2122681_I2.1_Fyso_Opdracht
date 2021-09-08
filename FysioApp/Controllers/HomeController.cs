da84b8f7039386276d1628e15391adef94b308bc
Draakedan 2 <amchelle@avans.nl>
2021-09-08 22:07:22 +0200

    [Added] Home and form pages
    home page show the patients and a link to the form

--- FysioApp/Controllers/HomeController.cs
+++ FysioApp/Controllers/HomeController.cs
@@ -20,6 +20,21 @@ namespace FysioApp.Controllers
 
         public IActionResult Index()
         {
+            return View(GeneratePatientList());
+        }
+
+        private static List<Patient> GeneratePatientList() 
+        {
+            List<Patient> patients = new()
+            {
+                new Patient("Kira", "012345", new DateTime(1998, 12, 13), DateTime.Now),
+                new Patient("Frank", "344660", new DateTime(1975, 5, 20), DateTime.Now)
+            };
+            return patients;
+        }
+
+        public IActionResult NewPatient()
+        {
             return View();
         }
 
---
+++ FysioApp/Models/Patient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FysioApp.Models
+{
+    public class Patient
+    {
+
+        public Patient(string Name, string ID, DateTime Birthdate, DateTime RegisterDate)
+        {
+            this.Name = Name;
+            this.ID = ID;
+            this.Birthdate = Birthdate;
+            this.RegisterDate = RegisterDate;
+        }
+
+        public string Name { get; }
+        public string ID { get; }
+        public DateTime Birthdate { get; }
+        public DateTime RegisterDate { get; }
+
+    }
+}
